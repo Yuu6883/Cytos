1487fa1611565d95092edaa1fb7a173abc2e6968
Yuu6883 <yuu6883dev@gmail.com>
2022-02-10 15:25:39 -0800

    Proxied save & restore in worker

--- source-js/render/worker.ts
+++ source-js/render/worker.ts
@@ -1,6 +1,12 @@
+import { initServerDB, loadServer, saveServer } from './client/state';
 import { CytosInputData, CytosTimings, CytosVersion } from './types';
 
 const ctx: Worker = self as any; // eslint-disable-line no-restricted-globals
+
+interface SaveResult {
+    mode: string;
+    buffer: Uint8Array;
+}
 
 interface CytosAddon {
     setInput(data: CytosInputData);
@@ -13,7 +19,12 @@ interface CytosAddon {
 
     getTimings: () => CytosTimings;
     getVersion: () => CytosVersion;
+
+    save: () => SaveResult;
+    restore: (mode: string, buffer: Uint8Array) => boolean;
 }
+
+let db: IDBDatabase;
 
 const Cytos: CytosAddon = eval("require('../../build/Release/cytos-addon.node')");
 
@@ -24,29 +35,56 @@ Cytos.onInfo(info => ctx.postMessage(info));
 
 setInterval(() => ctx.postMessage({ timings: Cytos.getTimings() }), 1000);
 
-ctx.addEventListener(
-    'message',
-    (
-        e: MessageEvent<{
-            mode?: string;
-            threads?: number;
-            isBenchmark?: boolean;
-            input?: CytosInputData;
-        }>,
-    ) => {
-        const { data } = e;
-        if (!data) return;
-        const { mode, threads, isBenchmark, input } = data;
+initServerDB().then(o => (db = o));
 
-        if (mode !== undefined) {
-            postMessage({ mode });
-            if (mode && isBenchmark) {
-                Cytos.setGameMode(`bench-${mode}`);
-            } else Cytos.setGameMode(mode);
+type WorkerDataEvent = MessageEvent<{
+    save?: boolean;
+    restore?: string;
+    mode?: string;
+    threads?: number;
+    isBenchmark?: boolean;
+    input?: CytosInputData;
+}>;
+
+const afterSave = async (result: SaveResult) => {
+    if (result) {
+        await saveServer(db, result.mode, result.buffer);
+        postMessage({ save: result.mode, bytes: result.buffer.byteLength });
+    }
+};
+
+ctx.addEventListener('message', async (e: WorkerDataEvent) => {
+    const { data } = e;
+    if (!data || !db) return;
+
+    const { save, restore, mode, threads, input } = data;
+
+    if (mode !== undefined) {
+        const result = Cytos.save();
+
+        postMessage({ mode });
+        Cytos.setGameMode(mode);
+
+        if (mode) {
+            const data = await loadServer(db, mode);
+            if (data) {
+                const restore = Cytos.restore(mode, data);
+                if (restore) postMessage({ restore: mode, bytes: data.byteLength });
+            }
         }
-        if (!isNaN(threads)) Cytos.setThreads(threads);
-        if (input) Cytos.setInput(input);
-    },
-);
+
+        await afterSave(result);
+    }
+    if (!isNaN(threads)) Cytos.setThreads(threads);
+    if (input) Cytos.setInput(input);
+
+    if (save) afterSave(Cytos.save());
+
+    if (restore) {
+        const data = await loadServer(db, restore);
+        const result = Cytos.restore(restore, data);
+        if (result) postMessage({ restore, bytes: data.byteLength });
+    }
+});
 
 export default 0;
