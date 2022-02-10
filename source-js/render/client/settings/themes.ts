9549e5547c0bb1620e3670ef2f991278c66a041d
Yuu6883 <yuu6883dev@gmail.com>
2022-02-10 15:28:11 -0800

    Added back autotheme and showInactiveTabBorder

--- source-js/render/client/settings/themes.ts
+++ source-js/render/client/settings/themes.ts
@@ -1,9 +1,11 @@
-import { getCssVar, setCssVar } from '../../react/utils';
+import { setCssVar } from '../../react/utils';
 import { Setting } from './setting';
 import { S } from './settings';
 
 const s = {
+    autoTheme: new S('autoTheme', true),
     showBorder: new S('showBorder', true),
+    showInactiveTabBorder: new S('showInactiveTabBorder', true),
     activeTabBorder: new S('activeTabBorder', '#ac2a3d'),
     inactiveTabBorder: new S('inactiveTabBorder', '#ffffff'),
     ejectColor: new S('ejectColor', '#e8385e'),
@@ -15,19 +17,27 @@ const s = {
 };
 
 export const ThemeList: Setting<string | boolean>[] = [
+    new Setting(s.autoTheme, 0, {
+        text: 'Auto Theme',
+    }),
     new Setting(s.showBorder, 0, {
         text: 'Show Cell Border',
     }),
+    new Setting(s.showInactiveTabBorder, 0, {
+        text: 'Show Inactive Tab Border',
+        dep: ['showBorder'],
+    }),
     new Setting(s.activeTabBorder, 2, {
         text: 'Active Tab Border',
-        dep: ['showBorder'],
+        dep: ['showBorder', '!autoTheme'],
     }),
     new Setting(s.inactiveTabBorder, 2, {
         text: 'Inactive Tab Border',
-        dep: ['showBorder'],
+        dep: ['showBorder', 'showInactiveTabBorder'],
     }),
     new Setting(s.ejectColor, 2, {
         text: 'Ejected Cell Color',
+        dep: ['!autoTheme'],
     }),
     new Setting(s.foodColor, 2, {
         text: 'Food Color',
