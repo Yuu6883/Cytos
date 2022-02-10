7dc53892bf38d0fabd1f824f06be7668ce246ec0
Yuu6883 <yuu6883dev@gmail.com>
2022-02-10 15:28:28 -0800

    display msg in chat

--- source-js/render/client/util/sys_message.ts
+++ source-js/render/client/util/sys_message.ts
@@ -1,12 +1,12 @@
 import { ChatType, sysMsg } from '../../stores/hud';
 
 export const SYS = {
+    msg: (msg: string) => sysMsg(ChatType.GAME, msg),
     gameConnected: (server: string) => sysMsg(ChatType.NONE, `Connected to $0`, server),
     gameDisconnected: (server: string, reason?: string) =>
         reason
             ? sysMsg(ChatType.NONE, 'Disconnected from $0: $1', server, reason)
             : sysMsg(ChatType.NONE, 'Disconnected from $0', server),
-    gameMsg: (msg: string) => sysMsg(ChatType.GAME, msg),
     webglContextLost: () =>
         sysMsg(ChatType.SYSTEM, 'WebGL context lost, reinitializing...'),
     renderError: () =>
