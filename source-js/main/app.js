adc2d67727133f0aabf09640d2c51983b9093194
Yuu6883 <yuu6883dev@gmail.com>
2022-02-13 13:08:53 -0800

    Removed unused code

--- source-js/main/app.js
+++ source-js/main/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const electron = require('electron');
-const { app, ipcMain, globalShortcut, BrowserWindow } = electron;
+const { app, globalShortcut, BrowserWindow } = electron;
 
 const ICON_PATH = path.resolve(__dirname, '..', 'cytos.ico');
 
@@ -35,13 +35,6 @@ class CytosMain {
             .on('activate', () => !this.window && createMainWindow())
             .on('before-quit', () => this.beforeQuit())
             .on('quit', () => this.onQuit());
-
-        ipcMain
-            .on('minimize', () => this.window && this.window.hide())
-            .on('toggle', () => this.toggle())
-            .on('close', () => this.closeMain())
-            .on('quit', () => this.quit())
-            .on('restart', () => (app.relaunch(), app.exit(0)));
     }
 
     createMainWindow() {
@@ -53,8 +46,8 @@ class CytosMain {
             // transparent: true,
             center: true,
             closable: true,
-            minimizable: false,
-            maximizable: false,
+            minimizable: true,
+            maximizable: true,
             show: false,
             resizable: true,
             minWidth: 1280,
@@ -73,12 +66,11 @@ class CytosMain {
         this.window.loadFile(path.join(__dirname, '..', 'build', 'index.html'));
         this.window.webContents.once('did-finish-load', () => {
             console.log('Window content loaded');
-            // this.mainWindow.removeMenu();
+            this.window.removeMenu();
             this.window.webContents.setZoomFactor(1);
             this.window.show();
         });
 
-        this.window.on('close', e => this.alertQuit(e));
         this.window.on('focus', () => {
             globalShortcut.register('CommandOrControl+R', _ => {});
             globalShortcut.register('CommandOrControl+Shift+R', _ => {});
@@ -96,36 +88,6 @@ class CytosMain {
         this.window.destroy();
         app.quit(0);
         process.exit(0);
-    }
-
-    /** @param {Electron.Event} e */
-    alertQuit(e) {
-        if (!this.window) return;
-        this.window.show();
-        this.window.focus();
-        this.window.center();
-        this.window.webContents.send('alertQuit');
-
-        e.preventDefault();
-    }
-
-    toggle() {
-        if (!this.window) return;
-
-        this.window.isMaximized() ? this.window.unmaximize() : this.window.maximize();
-        this.window.movable = !this.window.isMaximized();
-    }
-
-    showMain() {
-        this.window.flashFrame(true);
-        setTimeout(() => this.window.flashFrame(false), 3000);
-        this.window.show();
-        this.window.restore();
-        this.window.focus();
-    }
-
-    closeMain() {
-        this.window && this.window.hide();
     }
 
     beforeQuit() {}
